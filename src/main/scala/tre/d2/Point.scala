3a0f1f9bb2def9581bf3db6f70f286cf42b72803
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-06-02 23:17:00 -0600

    added 2d point

--- src/main/scala/tre/d2/Point.scala
+++ src/main/scala/tre/d2/Point.scala
@@ -1,59 +1,55 @@
-package tre.d3
+package tre.d2
 
 import Math._;
 
 /**
   * Created by francoponticelli on 6/2/16.
   */
-case class Point(x: Double, y: Double, z: Double) {
+case class Point(x: Double, y: Double) {
   lazy val length = sqrt(lengthSquared)
-  lazy val lengthSquared = x * x + y * y + z * z
+  lazy val lengthSquared = x * x + y * y
 
-  def abs() = Point(Math.abs(x), Math.abs(y), Math.abs(z))
+  def abs() = Point(Math.abs(x), Math.abs(y))
   def interpolate(that: Point)(d: Double) = (this + (that - this) * d)
   def isZero() = this == Point.zero
   def normalize() = this / length
 
   def distance(that: Point) = Math.abs((this - that).length)
   def distanceSquared(that: Point) = Math.abs((this - that).lengthSquared)
-  def cross(that : Point) = Point(
-        y * that.z - z * that.y,
-        z * that.x - x * that.z,
-        x * that.y - y * that.x
-      )
 
   def min(that : Point) = Point(
-        Math.min(x, that.x),
-        Math.min(y, that.y),
-        Math.min(z, that.z)
-      )
+    Math.min(x, that.x),
+    Math.min(y, that.y)
+  )
 
   def max(that : Point) = Point(
-        Math.max(x, that.x),
-        Math.max(y, that.y),
-        Math.max(z, that.z)
-      )
+    Math.max(x, that.x),
+    Math.max(y, that.y)
+  )
 
-  def + (that: Point) = Point(x + that.x, y + that.y, z + that.z)
-  def + (d: Double) = Point(x + d, y + d, z + d)
+  def to3DPoint(z: Double) = tre.d3.Point(x, y, z)
+
+  def + (that: Point) = Point(x + that.x, y + that.y)
+  def + (d: Double) = Point(x + d, y + d)
   def - (that: Point) = this + -that;
-  def - (d: Double) = Point(x - d, y - d, z - d)
-  def * (that: Point) = Point(x * that.x, y * that.y, z * that.z)
-  def * (d: Double) = Point(x * d, y * d, z * d)
-  def dot (that: Point) = x * that.x + y * that.y + z * that.z
-  def / (that: Point) = Point(x / that.x, y / that.y, z / that.z)
-  def / (d: Double) = Point(x / d, y / d, z / d)
-  def unary_- () = Point(-x, -y, -z)
-  override def toString() = s"($x,$y,$z)"
+  def - (d: Double) = Point(x - d, y - d)
+  def * (that: Point) = Point(x * that.x, y * that.y)
+  def * (d: Double) = Point(x * d, y * d)
+  def dot (that: Point) = x * that.x + y * that.y
+  def / (that: Point) = Point(x / that.x, y / that.y)
+  def / (d: Double) = Point(x / d, y / d)
+  def unary_- () = Point(-x, -y)
+  override def toString() = s"($x,$y)"
 }
 
 object Point {
-  val zero = Point(0, 0, 0)
-  implicit def tuple2point(t : (Double, Double, Double)) = Point(t._1, t._2, t._3)
-  implicit def point2tuple(p : Point) = (p.x, p.y, p.z)
-  implicit def point3d2point2d(p : Point) = tre.d2.Point(p.x, p.y)
+  val zero = Point(0, 0)
+  implicit def tuple2point(t : (Double, Double)) = Point(t._1, t._2)
+  implicit def point2tuple(p : Point) = (p.x, p.y)
 }
 /*
+  cross product
+
   public function nearEquals(p : Point)
     return Math.abs(x - p.x) <= Floats.EPSILON && Math.abs(y - p.y) <= Floats.EPSILON && Math.abs(z - p.z) <= Floats.EPSILON;
 
