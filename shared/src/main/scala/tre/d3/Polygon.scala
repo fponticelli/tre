418156707e4f5827d0a1ba352b5064eed182306a
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-06-10 19:34:35 -0600

    added Polygon.triangles

--- shared/src/main/scala/tre/d3/Polygon.scala
+++ shared/src/main/scala/tre/d3/Polygon.scala
@@ -6,3 +6,13 @@ case class Polygon(vertices: List[Vertex]) extends Iterable[Vertex] {
 
   lazy val plane =
     Plane.fromPoints(vertices(0).position, vertices(1).position, vertices(2).position)
+
+  def triangles(): List[(Vertex, Vertex, Vertex)] = {
+    val first = vertices.head
+    (2 until vertices.length).foldLeft(List[(Vertex, Vertex, Vertex)]()) {
+      (lst: List[(Vertex, Vertex, Vertex)], index: Int) => {
+        lst :+ (first, vertices(index - 1), vertices(index))
+      }
+    }
+  }
+}
